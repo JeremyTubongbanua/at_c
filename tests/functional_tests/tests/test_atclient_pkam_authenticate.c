e939acecb29fecc097999b7461e5be2e6a04b1d2
XavierChanth <xchanthavong@gmail.com>
2025-01-03 20:23:50 -0500

    chore: formatting

--- tests/functional_tests/tests/test_atclient_pkam_authenticate.c
+++ tests/functional_tests/tests/test_atclient_pkam_authenticate.c
@@ -58,33 +58,35 @@ static int test1_pkam_no_options() {
   atclient_authenticate_options authenticate_options;
   atclient_authenticate_options_init(&authenticate_options);
 
-  if((ret = atclient_authenticate_options_set_atdirectory_host(&authenticate_options, ATDIRECTORY_HOST)) != 0) {
-    return ret;
+  if ((ret = atclient_authenticate_options_set_atdirectory_host(&authenticate_options, ATDIRECTORY_HOST)) != 0) {
+    goto exit;
   }
 
-  if((ret = atclient_authenticate_options_set_atdirectory_port(&authenticate_options, ATDIRECTORY_PORT)) != 0) {
-    return ret;
+  if ((ret = atclient_authenticate_options_set_atdirectory_port(&authenticate_options, ATDIRECTORY_PORT)) != 0) {
+    goto exit;
   }
 
   if ((ret = atclient_atkeys_file_from_path(&atkeys_file, atkeyspath)) != 0) {
-    return ret;
+    goto exit;
   }
   atlogger_log(tag, ATLOGGER_LOGGING_LEVEL_INFO, "atclient_atkeys_file_from_string: %d\n", ret);
 
   if ((ret = atclient_atkeys_populate_from_atkeys_file(&atkeys, &atkeys_file)) != 0) {
-    return ret;
+    goto exit;
   }
   atlogger_log(tag, ATLOGGER_LOGGING_LEVEL_INFO, "atclient_atkeys_populate_from_atkeys_file: %d\n", ret);
 
   if ((ret = atclient_pkam_authenticate(&atclient, atsign, &atkeys, &authenticate_options, NULL)) != 0) {
     atlogger_log(tag, ATLOGGER_LOGGING_LEVEL_ERROR, "Failed to authenticate\n");
-    return ret;
+    goto exit;
   } else {
     atlogger_log(tag, ATLOGGER_LOGGING_LEVEL_DEBUG, "Authenticated\n");
   }
 
+exit: {
   atclient_authenticate_options_free(&authenticate_options);
   return ret;
+}
 }
 
 static int test2_pkam_with_options() {
@@ -116,8 +118,7 @@ static int test2_pkam_with_options() {
   }
   atlogger_log(tag, ATLOGGER_LOGGING_LEVEL_INFO, "atclient_atkeys_populate_from_atkeys_file: %d\n", ret);
 
-  if ((ret = atclient_utils_find_atserver_address(ATDIRECTORY_HOST,
-                                                  ATDIRECTORY_PORT, atsign, &atserver_host,
+  if ((ret = atclient_utils_find_atserver_address(ATDIRECTORY_HOST, ATDIRECTORY_PORT, atsign, &atserver_host,
                                                   &atserver_port)) != 0) {
     atlogger_log(tag, ATLOGGER_LOGGING_LEVEL_ERROR, "atclient_utils_find_atserver_address: %d\n", ret);
     return ret;
