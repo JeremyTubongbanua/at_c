62f85a597818fdfd0ed1199c41a5fc439350f124
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2025-01-07 03:35:02 +0000

    feat: add more fields to the functional tests

--- tests/functional_tests/lib/include/functional_tests/config.h
+++ tests/functional_tests/lib/include/functional_tests/config.h
@@ -26,6 +26,26 @@ extern "C" {
 #define SECOND_ATSIGN "@12snowboating"
 #endif
 
+// you can change this value at compile time by using `-DFIRST_ATSIGN_ATSERVER_HOST="vip.ve.atsign.zone"` in the CMake configure command
+#ifndef FIRST_ATSIGN_ATSERVER_HOST
+#define FIRST_ATSIGN_ATSERVER_HOST "228aafb0-94d3-5aa2-a3b3-e36af115480d.swarm0002.atsign.zone"
+#endif
+
+// you can change this value at compile time by using `-DFIRST_ATSIGN_ATSERVER_PORT=25000` in the CMake configure command
+#ifndef FIRST_ATSIGN_ATSERVER_PORT
+#define FIRST_ATSIGN_ATSERVER_PORT 6943
+#endif
+
+// you can change this value at compile time by using `-DSECOND_ATSIGN_ATSERVER_HOST="vip.ve.atsign.zone"` in the CMake configure command
+#ifndef SECOND_ATSIGN_ATSERVER_HOST
+#define SECOND_ATSIGN_ATSERVER_HOST "96424e26-f67f-5834-84a4-220b47fd7843.swarm0002.atsign.zone"
+#endif
+
+// you can change this value at compile time by using `-DSECOND_ATSIGN_ATSERVER_PORT=25000` in the CMake configure command
+#ifndef SECOND_ATSIGN_ATSERVER_PORT
+#define SECOND_ATSIGN_ATSERVER_PORT 6942
+#endif
+
 /**
  * @brief Get the atkeys file path for the given atSign.
  *
